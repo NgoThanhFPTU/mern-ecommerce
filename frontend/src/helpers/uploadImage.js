b11ea675de92ef20529dfe997bd0eff0c9242867
Le <nevergiveup2025.qn@gmail.com>
2025-03-24 11:55:46 +0700

    logo và giá tiền

--- frontend/src/helpers/uploadImage.js
+++ frontend/src/helpers/uploadImage.js
@@ -1,18 +1,23 @@
-const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`
+const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`;
 
-const uploadImage  = async(image) => {
-    const formData = new FormData()
-    formData.append("file",image)
-    formData.append("upload_preset","mern_product")
-    
+const uploadImage = async (image) => {
+  if (!image) {
+    console.error("No image provided!");
+    return null;
+  }
 
-    const dataResponse = await fetch(url,{
-        method : "post",
-        body : formData
-    })
+  console.log("Uploading image:", image.name);
 
-    return dataResponse.json()
+  const formData = new FormData();
+  formData.append("file", image);
+  formData.append("upload_preset", "mern_product");
 
-}
+  const dataResponse = await fetch(url, {
+    method: "post",
+    body: formData,
+  });
 
-export default uploadImage 
+  return dataResponse.json();
+};
+
+export default uploadImage;
